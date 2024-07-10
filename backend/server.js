b39b51b9e069f23c150285fc7c8c54cf8f376c23
AkshaySodhi <95957791+AkshaySodhi@users.noreply.github.com>
2024-07-10 19:35:29 +0530

    build setup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import cityRoutes from "./routes/city.routes.js";
@@ -9,6 +10,8 @@ import connectToDB from "./db/connectToDB.js";
 
 dotenv.config("");
 const PORT = process.env.PORT || 8000;
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -18,6 +21,12 @@ app.use(morgan("dev"));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/cities", cityRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   connectToDB();
